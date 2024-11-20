f83e563f5fa56afaaf07241aa8c8f96b06930e94
Dmitriy Shirokov <shirokov.d@autodoc.ru>
2024-11-20 20:25:02 +0300

    Click listeners added to main buttons.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.button_search)
+        val libraryButton = findViewById<Button>(R.id.button_library)
+        val settingsButton = findViewById<Button>(R.id.button_settings)
+
+        val buttonClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Переходим к поиску", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(buttonClickListener)
+
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Переходим в библиотеку", Toast.LENGTH_SHORT).show()
+        }
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Переходим к настройкам", Toast.LENGTH_SHORT).show()
+        }
     }
 }
