874061d590904ce8ae2188d8e8b63a9de555a31f
Dmitriy Shirokov <shirokov.d@autodoc.ru>
2024-11-26 18:05:46 +0300

    Added search, library and settings activities.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,33 +1,40 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_main)
         val searchButton = findViewById<Button>(R.id.button_search)
         val libraryButton = findViewById<Button>(R.id.button_library)
         val settingsButton = findViewById<Button>(R.id.button_settings)
 
+        toolbar.setNavigationIcon(null)
+
         val buttonClickListener = object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                Toast.makeText(this@MainActivity, "Переходим к поиску", Toast.LENGTH_SHORT).show()
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
             }
         }
         searchButton.setOnClickListener(buttonClickListener)
 
         libraryButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Переходим в библиотеку", Toast.LENGTH_SHORT).show()
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
         }
 
         settingsButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Переходим к настройкам", Toast.LENGTH_SHORT).show()
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
