1261c0ae9850e6dca60548636675ae9b841244cd
Dmitriy Shirokov <shirokov.d@autodoc.ru>
2025-01-21 19:56:34 +0300

    Sprint 9. Search field added.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,16 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +26,37 @@ class SearchActivity : AppCompatActivity() {
         findViewById<Toolbar>(R.id.toolbar_search).setNavigationOnClickListener {
             this.finish()
         }
+
+        val input = findViewById<EditText>(R.id.search_input)
+        val clearButton = findViewById<ImageView>(R.id.search_clear)
+
+        clearButton.setOnClickListener {
+            input.setText("")
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+                //empty
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+                clearButton.isVisible = !input.text.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //empty
+            }
+        }
+        input.addTextChangedListener(textWatcher)
     }
 }
