ab5f2f3057c52eadcbb33ae03fecd7cd0d545973
Dmitriy Shirokov <shirokov.d@autodoc.ru>
2025-01-22 20:33:14 +0300

    Sprint 9. Save and restore search query.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -14,6 +14,13 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
+    var searchQuery: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -39,28 +46,28 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val textWatcher = object : TextWatcher {
-            override fun beforeTextChanged(
-                p0: CharSequence?,
-                p1: Int,
-                p2: Int,
-                p3: Int
-            ) {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //empty
             }
 
-            override fun onTextChanged(
-                p0: CharSequence?,
-                p1: Int,
-                p2: Int,
-                p3: Int
-            ) {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 clearButton.isVisible = !searchField.text.isNullOrEmpty()
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                //empty
+                searchQuery = searchField.text.toString()
             }
         }
         searchField.addTextChangedListener(textWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchQuery)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        findViewById<EditText>(R.id.search_input).setText(savedInstanceState.getString(SEARCH_TEXT))
     }
 }
