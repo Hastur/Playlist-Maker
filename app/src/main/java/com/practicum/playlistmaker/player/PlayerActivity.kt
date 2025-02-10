e1f553c4de559d633b0d0659021c8acf466da9ba
DESKTOP-2J38U3B\Heimdallr <shirokov.d@autodoc.ru>
2025-02-11 01:13:19 +0300

    Sprint 13. Added player activity and layout with cover.

---
+++ app/src/main/java/com/practicum/playlistmaker/player/PlayerActivity.kt
@@ -0,0 +1,21 @@
+package com.practicum.playlistmaker.player
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.practicum.playlistmaker.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_player)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.player)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
